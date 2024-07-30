58522c3dbce84f529a8484360028a97bdd27a692
NickRest <nickrestemail@gmail.com>
2024-07-30 08:55:11 -0400

    set own port

--- src/main/java/social/nickrest/Main.java
+++ src/main/java/social/nickrest/Main.java
@@ -1,10 +1,42 @@
 package social.nickrest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import social.nickrest.server.SliceServer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        new SliceServer(8080).open();
+        Map<String, String> argMap = new Main().getArgumentMap(args);
+
+        if(argMap.containsKey("port")) {
+            try {
+                Integer.parseInt(argMap.get("port"));
+            } catch (NumberFormatException e) {
+                logger.error("Invalid port number (FAILED TO START SERVER)");
+                return;
+            }
+        }
+
+        new SliceServer(argMap.containsKey("port") ? Integer.parseInt(argMap.get("port")) : 8080).open();
+    }
+
+    public Map<String, String> getArgumentMap(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        for(int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if(arg.startsWith("--")) {
+                String key = arg.substring(2);
+                String value = args[i + 1];
+                map.put(key, value);
+                i++;
+            }
+        }
+        return map;
     }
 }
